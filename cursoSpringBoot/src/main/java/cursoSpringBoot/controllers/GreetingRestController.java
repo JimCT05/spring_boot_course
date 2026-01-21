cbe245584dcbb675d635645f557bc11af5aec06d
JimCT <partnerig11@gmail.com>
2026-01-21 10:43:54 +0100

    Primera subida del proyecto

---
+++ cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/GreetingRestController.java
@@ -0,0 +1,17 @@
+package cursoSpringBoot.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class GreetingRestController {
+
+    @GetMapping("/saludo/{name}")
+    public String greeting(@PathVariable String name){
+        return "Hello," + name;
+    }
+
+}
